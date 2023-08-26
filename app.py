8ec84d1f046be31a19437dbed65cefb27807b302
Maviyashek <ameermaviya098@gmail.com>
2023-08-26 16:56:31 +0530

    added register page

--- app.py
+++ app.py
@@ -1,29 +1,77 @@
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, redirect, url_for, request, render_template, current_app
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
+db = SQLAlchemy(app)
+
 
 # this is default username and password
-user_pass = {'username': 'username', 'password': 'password'}
+# user_pass = {'username': 'username', 'password': 'password'}
 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+
+# @app.route("/", methods=["GET", "POST"])
+# def login():
+#   if request.method == "POST":
+#     username = request.form.get("username")
+#     password = request.form.get("password")
+
+#     if username == user_pass['username'] and password == user_pass['password']:
+#       return redirect(url_for('welcome', username=username))
+#     else:
+#       return "Invalid credentials"
+
+#   return render_template('login.html')
+
+
+# @app.route("/Ai_links/<username>")
+# def welcome(username):
+#   return render_template('welcome.html', username=username)
 
 @app.route("/", methods=["GET", "POST"])
 def login():
-  if request.method == "POST":
-    username = request.form.get("username")
-    password = request.form.get("password")
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
 
-    if username == user_pass['username'] and password == user_pass['password']:
-      return redirect(url_for('welcome', username=username))
-    else:
-      return "Invalid credentials"
+        user = User.query.filter_by(username=username, password=password).first()
 
-  return render_template('login.html')
+        if user:
+            return redirect(url_for('welcome', username=username))
+        else:
+            return "Invalid credentials"
 
+    return render_template('login.html')
 
-@app.route("/Ai_links/<username>")
+@app.route("/welcome/<username>")
 def welcome(username):
-  return render_template('welcome.html', username=username)
+    return render_template('welcome.html', username=username)
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
 
 
 if __name__ == '__main__':
+  with app.app_context(): 
+    db.create_all()
   app.run(debug=True, host='0.0.0.0')
