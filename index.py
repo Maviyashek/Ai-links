f967a079d97e56b370f4895c31f8d5145e01d411
Maviyashek <ameermaviya098@gmail.com>
2023-08-27 16:50:45 +0530

    d

---
+++ index.py
@@ -0,0 +1,52 @@
+from flask import Flask, redirect, url_for, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+@app.route("/login.html", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(username=username, password=password).first()
+
+        if user:
+            return redirect(url_for('welcome', username=username))
+        else:
+            return "Invalid credentials"
+
+    return render_template('login.html')
+
+@app.route("/welcome/<username>")
+def welcome(username):
+    return render_template('welcome.html', username=username)
+
+@app.route("/register.html", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    with app.app_context(): 
+        db.create_all()
+    app.run(debug=True, host='0.0.0.0')
